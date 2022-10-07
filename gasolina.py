62271672173aad0b6f50ddb455ddd5e8060db046
Tobias-Melo <tobiasmellow@gmail.com>
2022-10-07 02:00:40 +0000

    Alteração no gráfico (acrescento de elementos... legenda, cor, nome de eixo, titulo, etc.)

--- gasolina.py
+++ gasolina.py
@@ -1,4 +1,26 @@
+# código de geração do gráfico
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 df = pd.read_csv('gasolina.csv')
 
-line_plot = sns.lineplot(x=df['dia'], y=df['venda']).get_figure()
-line_plot.savefig('gasolina.png')
+dia = df['dia']
+preco = df['venda']
+
+sns.lineplot(x=dia, y=preco, color = 'b', label = 'Preço')
+
+# Configurando título e rótulos dos eixos.
+plt.title('Preço da gasolina por dia', fontsize=14)
+plt.xlabel('Dia', fontsize=12)
+plt.ylabel('Preço (R$)', fontsize=12)
+
+# Mudando a grossura dos eixos.
+plt.tick_params(axis='both', labelsize=10)
+
+# Legenda no gráfico
+plt.legend()
+
+plt.savefig('gasolina.png')
+plt.show() # Para 'desenhar' o gráfico
